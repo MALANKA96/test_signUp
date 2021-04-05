ae4d1c9c9c831ff2294d5152ed0557fee85381d2
Николай Вокса <@bkp.local>
2021-04-05 15:56:02 +0300

    fix text validatora

--- src/validators/validatorsLogIn.js
+++ src/validators/validatorsLogIn.js
@@ -8,7 +8,7 @@ const validate = (values) => {
 const validationEmail = (values, errors) => {
   if (!values.email) {
     errors.email =
-      "enter ayour account email address";
+      "enter your account email address";
   } else if (
     !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
       values.email
